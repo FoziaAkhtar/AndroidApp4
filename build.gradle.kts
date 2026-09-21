6a889957801ff1db9936f7ae3f0504bf1b156cc4
Fozia Akhtar <12345678+FoziaAkhtar@users.noreply.github.com>
2026-09-21 09:21:53 -0400

    chore: prepare Assignment 8 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     alias(libs.plugins.android.application) apply false
+
+    // ============================================================
+    // KSP
+    // Kotlin Symbol Processing is required by Room Database.
+    // ============================================================
+
+    id("com.google.devtools.ksp") version "2.2.20-2.0.2" apply false
 }
